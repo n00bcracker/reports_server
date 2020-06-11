738946c40d632b6a1e79c2e8f3d26584e806352e
kaktuz <zacpa@ya.ru>
2020-06-11 12:42:40 +0300

    Edited uploaded page, added handling upload huge file

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,6 @@ from config import UPLOAD_FOLDER, DOWNLOAD_FOLDER, FLASK_TEMPLATE_PATH
 app = Flask(__name__, template_folder=FLASK_TEMPLATE_PATH)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['DOWNLOAD_FOLDER'] = DOWNLOAD_FOLDER
-app.config['MAX_CONTENT_LENGTH'] = 5 * 1024 * 1024 # 5Mb
+app.config['MAX_CONTENT_LENGTH'] = 100 * 1024 * 1024 # 100Mb
 
 from app import routes
